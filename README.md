14bb820f8e691bb45785d780d2e250e6afbeba60
Li Lin <guru.lin@gmail.com>
2016-11-03 02:30:35 +0800

    update run script and doc

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Docker containers for Atlassian JIRA, Stash, Fisheye and Bamboo
+# Docker containers for Atlassian JIRA, Stash and Bamboo
 
 These containers are intended to run behind BASE_URL served Apache httpd.
 
@@ -8,10 +8,20 @@ These containers are intended to run behind BASE_URL served Apache httpd.
 One liner to get everything up and running,
 
 ```
-# if using boot2docker, replace 127.0.0.1 with the IP of the boot2docker VM
+#
+# on a machine that has docker installed
+#
+
+# this the URL where application can be accessed from
 export BASE_URL=http://127.0.0.1
 
-curl -sL https://raw.githubusercontent.com/sloppycoder/atlassian-dockers/master/run.sh | bash -
+# create data directory to store application state
+export DATA_DIR=/data # or your own directory name
+mkdir -p $DATA_DIR/postgres
+mkdir -p $DATA_DIR/atl_home
+
+sh run.sh all
+
 ```
 
 
@@ -32,35 +42,8 @@ Then point your browser at the URL below to  install license and configuation fo
 |  JIRA    										   | BASE_URL/jira       |
 |  Stash    			   						   | BASE_URL/stash      |
 |  Bamboo    									   | BASE_URL/bamboo     |
-|  Fisheye (optional, not started by default)      | BASE_URL/fisheye    |
 
 To use the postgres database, use __db__ as host name, postgres as username and password as passord. Database name is same as application name, e.g jira, stash, bamboo, fisheye.
 
 
-### Test with boot2docker, Amazon EC2 instance and Google Compute Enginge
 
-#### boot2docker with Parallels on Mac
-
-The following steps should work for most people. 
-
-```
-cd vagrant/paralles
-vagrant up
-
-# copy the certificates files from ~/.docker in the VM to the host machine
-# modify docker-env script and update the DOCKER_CERT_PATH with directory above
-# then
-
-. ./docker-env
-
-```
-
-
-#### Amazon EC2 instance with Amazon Linux AMI (HVM)
-Before ```vagrant up``` please check the Vagrantfile to make sure it contains the configuration you want, especially the your access key, region, VPC subnet_id etc in order to work correctly. 
-
-#### Google Compute Engine with image "centos-7-v20150127"
-Before ```vagrant up``` please check the Vagrantfile to make sure it contains the configuration you want, especially the your aproject id, access client email and key location. For ssh access to the instance, please upload your ssh key via console before spin up the instance. 
-
-see [GCE documentation](https://cloud.google.com/compute/docs/console#sshkeys) for details. more references in [this GitHub issue](https://github.com/mitchellh/vagrant-google/issues/23)
- 
--- run.sh
+++ run.sh
@@ -1,36 +1,14 @@
 #! /bin/bash
 
-DOCKER_HUB_USER=sloppycoder
+DOCKER_REGISTRY_PREFIX=sloppycoder
 
-usage() {
+[ "$DATA_DIR" = "" ] && DATA_DIR=/data
 
-echo "
+POSTGRES_DATA=$DATA_DIR/postgres
+ATL_DATA=$DATA_DIR/atlassian-home
 
- Usage:
-
-    run.sh all             # 
-
-    run.sh <app>           # stop and remove <app> container, create a new one in background
-                           # then restart web server 
-                           # <app> can be jira, stash, fisheye or bamboo
-
-    run.sh <app> -i <cmd>  # start <app> container in interactive mode, pass <cmd> to docker
-
-    run.sh  web            # restart web server 
-
-    run.sh  db             # stop and remove db container, then start a new one
-
-    run.sh  dbsh           # launch a shell into db server
-
-    run.sh  datash         # start an interactive container for examine the shared data volumes
-
-    run.sh  backup <dir>   # backup all data inside data container into <dir>/atldata.tar.gz
-
-    run.sh  clean          # remoev all untagged images
-
-"
-
-}
+[ ! -d "$ATL_DATA" ] && echo "Unable to find data volume $ATL_DATA" && exit 1
+[ ! -d "$POSTGRES_DATA" ] && echo "Unable to find data volume $POSTGRES_DATA" && exit 1
 
 
 is_container_running() {
@@ -56,12 +34,10 @@ start_app() {
 
     stop_and_rm_container $1
 
-    start_data
-
     echo starting $1
     docker run $RUN_MODE --name $1 --link postgres:db \
-        --volumes-from="atldata" -e BASE_URL=$BASE_URL \
-        ${DOCKER_HUB_USER}/atl-$1 $3
+        --volume=$ATL_DATA:/opt/atlassian-home -e BASE_URL=$BASE_URL \
+        ${DOCKER_REGISTRY_PREFIX}/atl-$1 $3
 }
 
 
@@ -69,20 +45,8 @@ start_db() {
 
     stop_and_rm_container postgres
 
-    start_data
-
     docker run -d --name postgres -e POSTGRES_PASSWORD=password \
-        --volumes-from="atldata" ${DOCKER_HUB_USER}/atl-postgres
-}
-
-
-start_data() {
-
-    (docker ps -a | grep -q atldata ) || \
-        docker run --name atldata \
-            -v /opt/atlassian-home \
-            -v /var/lib/postgresql/data \
-            busybox echo atldata container
+        --volume=$POSTGRES_DATA:/var/lib/postgresql/data ${DOCKER_REGISTRY_PREFIX}/atl-postgres
 }
 
 
@@ -104,15 +68,14 @@ start_web() {
 
     is_container_running jira && LINKS="$LINKS --link jira:jira"
     is_container_running stash && LINKS="$LINKS --link stash:stash"
-    is_container_running fisheye && LINKS="$LINKS --link fisheye:fisheye"
     is_container_running bamboo && LINKS="$LINKS --link bamboo:bamboo"
 
     echo starting httpd with links $LINKS
-    stop_and_rm_container atlweb
-    docker run -d --name atlweb \
+    stop_and_rm_container httpd
+    docker run -d --name httpd \
        $PORTS \
        $LINKS \
-       ${DOCKER_HUB_USER}/atl-web
+       ${DOCKER_REGISTRY_PREFIX}/atl-web
 
     [ -z "$LINKS" ] && echo httpd started but no application running.
 }
@@ -142,17 +105,15 @@ fi
 case "$ACTION" in
 
     all)
-        start_data
         start_db
         start_app jira
         start_app stash
- #       start_app fisheye
         start_app bamboo
         sleep 2 && start_web
 
     ;;
 
-    jira|stash|fisheye|bamboo)
+    jira|stash|bamboo)
         
         stop_and_rm_container $1
         start_app $1 $2 $3 $4
@@ -173,32 +134,18 @@ case "$ACTION" in
 
     ;;
 
-    initdb)
-        
-        init_db        
-
-    ;;
-
     dbsh)
 
         docker exec -it postgres /bin/bash
 
     ;;
 
-    datash)
-
-        docker run -it --rm --volumes-from="atldata" sloppycoder/jre-base /bin/bash
-
-    ;;
-
-    backup)
-
-        BACKUP_DIR=$(readlink -f $2)
-        echo backing up all data in atldata to $BACKUP_DIR
-
-        docker run -it --rm --volumes-from atldata -v $BACKUP_DIR:/backup  ${DOCKER_HUB_USER}/java-base  \
-              tar czvf /backup/atldata.tar.gz \
-                   /opt/atlassian-home /var/lib/postgresql/data 
+    kill)
+        stop_and_rm_container httpd
+        stop_and_rm_container bamboo
+        stop_and_rm_container jira
+        stop_and_rm_container stash
+        stop_and_rm_container postgres
 
     ;;
 
@@ -210,8 +157,28 @@ case "$ACTION" in
 
     *)
 
-        usage
+        echo "
 
+ Usage:
+
+    run.sh all             # start everything
+
+    run.sh <app>           # stop and remove <app> container, create a new one in background
+                           # then restart web server 
+                           # <app> can be jira, stash or bamboo
+
+    run.sh <app> -i <cmd>  # start <app> container in interactive mode, pass <cmd> to docker
+
+    run.sh  web            # restart web server 
+
+    run.sh  db             # stop and remove db container, then start a new one
+
+    run.sh  dbsh           # launch a shell into db server
+
+    run.sh  kill           # stop and remove everything
+
+    run.sh  clean          # remoev all untagged images
+"
     ;;
 
 esac 
